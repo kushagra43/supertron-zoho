58b549a80b4a9955a630497ea74f4ff82c33534b
kushagra43 <63740432+kushagra43@users.noreply.github.com>
2023-01-11 13:43:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-document.querySelector("#myForm").addEventListener("submit", function (e) {
-  // Prevent default form submission behavior
-  e.preventDefault();
+// document.querySelector("#myForm").addEventListener("submit", function (e) {
+//   // Prevent default form submission behavior
+//   e.preventDefault();
 
-  // Show "Thank you" message
-  document.querySelector("#thankYou").style.display = "block";
+//   // Show "Thank you" message
+//   document.querySelector("#thankYou").style.display = "block";
 
-  // Remove form
-  this.remove();
-});
+//   // Remove form
+//   this.remove();
+// });
+
+
+              $("#myform").submit(function (e) {
+                e.preventDefault(); // avoid to execute the actual submit of the form.
+                var form = $(this);
+                var url = form.attr("action");
+                $.ajax({
+                  type: "POST",
+                  url: url,
+                  data: form.serialize(), // serializes the form's elements.
+                  success: function (data) {
+                    // Show "Thank you" message
+                    document.querySelector("#thankYou").style.display = "block";
+                      this.remove();
+                  },
+                  error: function (data) {
+                    alert("Something went wrong, Please try again later.");
+                  },
+                });
+              });
