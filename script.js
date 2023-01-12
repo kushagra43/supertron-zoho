fd1914f2ba246439f78b26a66a20ee5eef77f324
Mr.Nayra <aryanedu5786@gmail.com>
2023-01-12 10:12:13 +0530

    addition of google tag manager, form functionality

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 document.querySelector("#myForm").addEventListener("submit", function (e) {
-  // Prevent default form submission behavior
   e.preventDefault();
 
-  // Show "Thank you" message
-  document.querySelector("#thankYou").style.display = "block";
+  // Collect form data
+  let formData = new FormData(this);
 
-  // Remove form
-  this.remove();
+  // Send post request to the server
+  fetch("https://forms.hubspot.com/uploads/form/v2/23736002/688d8b8a-37c8-4bf1-bd94-9e2e31d4c0d8", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "success") {
+        // Show success message
+        document.querySelector("#thankYou").innerHTML =
+          "Thank you for submitting the form!";
+        document.querySelector("#thankYou").style.color = "green";
+        document.querySelector("#thankYou").style.display = "block";
+      } else {
+        // Show error message
+        document.querySelector("#thankYou").innerHTML =
+          "An error occurred while submitting the form. Please try again later.";
+        document.querySelector("#thankYou").style.color = "red";
+        document.querySelector("#thankYou").style.display = "block";
+      }
+
+      // Remove form
+      this.remove();
+    })
+    .catch((error) => {
+      console.error(error);
+      // Show error message
+      document.querySelector("#thankYou").innerHTML =
+        "Something went wrong, please try again later.";
+      document.querySelector("#thankYou").style.color = "red";
+      document.querySelector("#thankYou").style.display = "block";
+      // Remove form
+      this.remove();
+    });
 });
